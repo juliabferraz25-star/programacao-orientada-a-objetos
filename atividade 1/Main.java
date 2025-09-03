3e95bf01d0517a1b282c2af90a1656dc5396ac82
juliabferraz25-star <juliabferraz25@gmail.com>
2025-09-03 12:45:26 -0300

    add-main

---
+++ atividade 1/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main (String[] args) {
+
+        public Integer Computador;
+        public Integer programa;
+        public Integer sistemaOperacional;
+        
+    }
+    
+}
